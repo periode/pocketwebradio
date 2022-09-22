0bc3bf1eef8526041f35e6edf31537d7c3de6251
période <pierre.depaz@gmail.com>
2022-09-22 17:23:33 +0200

    added icons properly

--- PlaybackService.js
+++ PlaybackService.js
@@ -4,9 +4,5 @@ import TrackPlayer, { Event } from 'react-native-track-player';
 export async function PlaybackService() {
     TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
     TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
-    TrackPlayer.addEventListener(Event.RemoteNext, () =>{
-        console.log('service skip');
-        // TrackPlayer.add(stations[Math.floor(Math.random() * stations.length)])
-        // TrackPlayer.skipToNext()
-    });
+    TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext());
 };
